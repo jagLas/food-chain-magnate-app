7dd45d1a65f0fffeb7a3571c09c433e9d57a720d
jagLas <jglasher@gmail.com>
2023-08-28 11:23:45 -0400

    player model

---
+++ backend/app/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Player(db.Model):
+    __tablename__ = 'players'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
