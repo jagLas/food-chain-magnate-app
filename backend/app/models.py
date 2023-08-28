0ec4bda78b021addb29ad7e93d0f937e97e7bf84
jagLas <jglasher@gmail.com>
2023-08-28 13:22:47 -0400

    rounds and games model

--- backend/app/models.py
+++ backend/app/models.py
@@ -21,3 +21,37 @@ class Player(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    rounds = db.relationship('Round', back_populates='player')
+
+
+class Game(db.Model):
+    __tablename__ = 'games'
+
+    id = db.Column(db.Integer, primary_key=True)
+    bank_start = db.Column(db.Integer, nullable=False)
+    bank_reserve = db.Column(db.Integer)
+
+
+class Round(db.Model):
+    __tablename__ = 'rounds'
+
+    id = db.Column(db.Integer, primary_key=True)
+    round = db.Column(db.Integer, nullable=False)
+    player_id = db.Column(db.String(50), db.ForeignKey('players.id'))
+    first_burger = db.Column(db.Boolean)
+    first_pizza = db.Column(db.Boolean)
+    first_drink = db.Column(db.Boolean)
+    first_waitress = db.Column(db.Boolean)
+    cfo = db.Column(db.Boolean)
+    unit_price = db.Column(db.Integer, default=10, nullable=False)
+    waitresses = db.Column(db.Integer, default=0, nullable=False)
+    salaries_paid = db.Column(db.Integer, default=0, nullable=False)
+
+    # # for future development. Columns to auto calculate salary discount
+    # first_to_train = db.Column(db.Boolean)
+    # first_billboard = db.Column(db.Boolean)
+    # unused_hr = db.Column(db.Integer, default=0, nullable=False)
+    # marketers = db.Column(db.Integer, default=0, nullable=False)
+
+    player = db.relationship('Player', back_populates='rounds')
