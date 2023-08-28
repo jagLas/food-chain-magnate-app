0489591ec044f7e89810426f278e4fd238cd5d03
jagLas <jglasher@gmail.com>
2023-08-28 12:30:29 -0400

    player model and alembic init

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask, jsonify
+from flask_migrate import Migrate
+from app.models import db
 from .config import Configuration
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+db.init_app(app)
+migrate = Migrate(app, db)
+
 
 @app.route('/')
 def index():
