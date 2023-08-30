563727350c31516d4efb0a66c054e5709780a007
jagLas <jglasher@gmail.com>
2023-08-30 13:03:23 -0400

    changed seed data

--- backend/app/routes/seed.py
+++ backend/app/routes/seed.py
@@ -18,11 +18,11 @@ def seed_db():
     db.session.add(game)
 
     round1 = Round(game=game, round=1, player=player1, first_waitress=True,
-                   waitresses=2, cfo=True)
+                   waitresses=2, first_drink=True, cfo=True)
     round2 = Round(game=game, round=1, player=player2, first_burger=True,
                    unit_price=9, waitresses=1)
     round3 = Round(game=game, round=2, player=player1, first_waitress=True,
-                   waitresses=1, cfo=True)
+                   waitresses=1, first_drink=True, cfo=True)
     round4 = Round(game=game, round=2, player=player2, first_burger=True,
                    first_pizza=True, unit_price=9, waitresses=1, cfo=True)
 
@@ -33,8 +33,8 @@ def seed_db():
                  garden=True)
     sale5 = Sale(round=round1, house_number=3, burgers=2, drinks=1)
     sale2 = Sale(round=round2, house_number=5, pizzas=2, drinks=2)
-    sale3 = Sale(round=round3, house_number=5, drinks=1)
-    sale4 = Sale(round=round4, house_number=1, burgers=1)
+    sale3 = Sale(round=round3, house_number=5, drinks=2)
+    sale4 = Sale(round=round4, house_number=1, burgers=1, drinks=1, pizzas=3)
     db.session.add_all([sale1, sale2, sale3, sale4, sale5])
 
     db.session.commit()
