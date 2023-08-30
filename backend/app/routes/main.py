bc58570a127387720cdafab7b169ff37ebc123e8
jagLas <jglasher@gmail.com>
2023-08-30 12:56:37 -0400

    progress on revenue calcs

--- backend/app/routes/main.py
+++ backend/app/routes/main.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from ..models import db, Game, Player, Round, Sale
 from sqlalchemy.sql import functions as func
-from sqlalchemy import case
+from sqlalchemy import case, and_, select
 
 bp = Blueprint('main', __name__)
 
@@ -50,8 +50,8 @@ def get_rounds(id):
 
 @bp.route('/games/<int:id>/bank')
 def get_bank(id):
-    # rounds = Sale.query.join(Round).filter_by(game_id=id).all()
-    # print(rounds)
+
+    # Case to calculate revenue from sales accounting for gardens
     sales_case = case(
         (Sale.garden == True, Round.unit_price * 2 * (Sale.burgers +
                                                       Sale.pizzas +
@@ -59,31 +59,71 @@ def get_bank(id):
         else_=Round.unit_price * (Sale.burgers + Sale.pizzas + Sale.drinks)
     )
 
+    # Case to calculate waitress income accounting for first waitress milestone
     waitress_case = case(
         (Round.first_waitress == True, Round.waitresses * 5),
-        else_= Round.waitresses * 3
+        else_=Round.waitresses * 3
     )
 
-    sales_totals = db.session.query(
-        (Round.game_id).label('game_id'),
-        # (Sale.round_id).label('round_id'),
-        # (Sale.garden).label('garden'),
-        # func.sum(Sale.burgers).label('burger_total'),
-        # func.sum(Sale.pizzas).label('pizza_total'),
-        # func.sum(Sale.drinks).label('drink_total'),
-        func.sum(sales_case.label('revenue')).label('revenue_total')
-        ).select_from(Sale).join(Sale.round) \
-        .group_by(Round.game_id).filter_by(game_id=id).all()
+    # Subquery to that will be used to combine waitress income with sales
+    waitresses_subquery = db.session.query(
+            (Round.game_id).label('game_id'),
+            (Round.id).label('round_id'),
+            func.sum(waitress_case).label('waitresses')
+        ).filter_by(game_id=id).group_by(Round.game_id, Round.id).subquery()
 
-    waitress_totals = db.session.query(
-        (Round.game_id).label('game_id'),
-        func.sum(waitress_case.label('revenue')).label('revenue_total')
-        ).group_by(Round.game_id).filter_by(game_id=id).all()
+    # Query for necessary info to make calculations
+    rounds = db.session.query(
+            (Round.game_id).label('game_id'),
+            (Sale.round_id).label('round_id'),
+            Round.first_burger,
+            Round.first_pizza,
+            Round.first_drink,
+            func.sum(Sale.burgers).label('burgers'),
+            func.sum(Sale.pizzas).label('pizzas'),
+            func.sum(Sale.drinks).label('drinks'),
+            func.sum(sales_case).label('sales'),
+            waitresses_subquery.c.waitresses
+        ).select_from(Sale).join(Round).group_by(
+            Round.game_id,
+            Sale.round_id,
+            Round.first_burger,
+            Round.first_pizza,
+            Round.first_drink,
+            waitresses_subquery.c.waitresses
+        ).filter_by(game_id=id)\
+        .join(
+            waitresses_subquery,
+            Sale.round_id == waitresses_subquery.c.round_id
+        ).all()
 
-    print(sales_totals)
-    print(waitress_totals)
+    # helper function to calculate "first" milestone bonuses
+    def calc_milestone_bonus(obj):
+        total = 0
+        if obj.first_burger is True:
+            total += obj.burgers * 5
+        if obj.first_pizza is True:
+            total += obj.pizzas * 5
+        if obj.first_drink is True:
+            tota += obj.drinks * 5
+        return total
 
-    return 'bank route'
+    # turn into dict to return as json
+    round_info = [{
+        'game_id': round.game_id,
+        'round_id': round.round_id,
+        'first_burger': round.first_burger,
+        'first_pizza': round.first_pizza,
+        'first_drink': round.first_drink,
+        'sales': round.sales,
+        'burgers': round.burgers,
+        'pizzas': round.pizzas,
+        'drinks': round.drinks,
+        'waitresses': round.waitresses,
+        'milestone_bonus': calc_milestone_bonus(round)
+    } for round in rounds]
+
+    return round_info
 
 
 @bp.route('/players',)
