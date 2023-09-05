a4a137ae68119ace726a9d4a6f705adc63a90ab2
jagLas <jglasher@gmail.com>
2023-09-05 11:01:39 -0400

    capitalized total

--- frontend/src/components/TotalRow.js
+++ frontend/src/components/TotalRow.js
@@ -1,7 +1,7 @@
 const TotalRow = ({obj}) => {
     return (
         <tr className={obj.player_id ? 'player-row' : 'total-row'}>
-            <td>{obj.name ? obj.name : 'total'}</td>
+            <td>{obj.name ? obj.name : 'Total'}</td>
             <td>{obj.total_revenue}</td>
             <td>{obj.total_expenses}</td>
             <td>{obj.total_income}</td>
