5945bc589c3bb40f96e867fc0c7ae0212bbee486
jagLas <jglasher@gmail.com>
2023-09-06 10:46:13 -0400

    added some routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import Totals from './components/Totals';
 import Rounds from './components/Rounds';
 import CreateGameForm from './components/CreateGame/CreateGame';
+import {Link, Route, Routes} from 'react-router-dom'
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
+
     <div className="App">
-      <CreateGameForm />
-      <Totals />
-      <Rounds />
+      <h1>Food Chain Companion App</h1>
+      <Routes>
+        <Route path='/create-game' element={<CreateGameForm />}></Route>
+        <Route path='/games/:id' element={<><Totals /><Rounds /></>}></Route>
+        <Route path='/' element={<LandingPage />}></Route>
+      </Routes>
     </div>
+
   );
 }
 
---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+const LandingPage = () => {
+    return (
+        <>
+            <ul>
+                <li><Link to={'/create-game'}>Create Game</Link></li>
+                <li><Link to={'/games'}>Load Game</Link></li>
+                <li><Link to={'/players'}>View Players</Link></li>
+                <li><Link to={'/create-player'}>Create Player</Link></li>
+            </ul>
+
+        </>
+    )
+}
+
+export default LandingPage
