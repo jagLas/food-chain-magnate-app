b941d77c04974a2f9afaaa25a7723786ea4f281f
jagLas <jglasher@gmail.com>
2023-09-08 14:59:15 -0400

    eror handler to json

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, json
 from flask_cors import CORS
 from flask_migrate import Migrate
 from app.models import db
 from .config import Configuration
 from .routes import main, seed
+from werkzeug.exceptions import HTTPException
 
 # initialize Flask app
 app = Flask(__name__)
@@ -22,3 +23,18 @@ migrate = Migrate(app, db)
 # register routes
 app.register_blueprint(main.bp)
 app.register_blueprint(seed.bp)
+
+
+# turns errors into json responses
+@app.errorhandler(HTTPException)
+def not_found(error):
+    response = error.get_response()
+    response.data = json.dumps({
+        'code': error.code,
+        'name': error.name,
+        'description': error.description
+    })
+
+    response.content_type = 'application/json'
+
+    return response
