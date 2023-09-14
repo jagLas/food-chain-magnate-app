ffec73244013cdc39d3a928bf005712346ceb017
jagLas <jglasher@gmail.com>
2023-09-14 11:00:00 -0400

    added form handler to update bank reserve

--- frontend/src/components/GameView/Bank.js
+++ frontend/src/components/GameView/Bank.js
@@ -1,12 +1,57 @@
-import { useGame } from "../GameContext"
+import { useParams } from "react-router-dom"
+import { useGame, useGameDispatch } from "../GameContext"
+import { useEffect, useState } from "react"
+import { actions } from "../GameReducer"
 
 export default function Bank () {
     const {bank} = useGame()
+    const [reserve, setReserve] = useState(bank.reserve)
+    const {gameId} = useParams()
+    const dispatch = useGameDispatch();
+
+    // updates the form field after bank.reserve has been retrieved
+    useEffect(() => {
+        setReserve(bank.reserve)
+    }, [bank.reserve])
+
+    const onChangeHandler = (e) => {
+        setReserve(e.target.value)
+    }
+
+    const onBlurHandler = async () => {
+        let data = await fetch(`${process.env.REACT_APP_DB_URL}/games/${gameId}/bank`, {
+            method: 'PATCH',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                reserve
+            })
+        })
+
+        data = await data.json()
+
+        dispatch({
+            type: actions.UPDATE_BANK_RESERVE,
+            payload: data
+        })
+    }
+
     return (
         <>
-            <span>Start: {bank ? bank.start : 0}</span>
-            <span>Reserve: {bank ? bank.reserve : 0}</span>
-            <span>Remaining: {bank ? bank.total : 0}</span>
+            <span>Start: {bank.start}</span>
+            <label>Reserve:
+                <input
+                    value={reserve}
+                    type='number'
+                    step={100}
+                    min={0}
+                    max={1800}
+                    onChange={onChangeHandler}
+                    onBlur={onBlurHandler}
+                ></input>
+            </label>
+            <span>Remaining: {bank.total}</span>
         </>
     )
 }
