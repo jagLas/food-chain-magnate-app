1840bc01460c5b2ef04b0b5dbd14be07c01966da
jagLas <jglasher@gmail.com>
2023-09-19 19:49:31 -0400

    buttons styled in menus

--- frontend/src/components/card-schemes.js
+++ frontend/src/components/card-schemes.js
@@ -14,10 +14,10 @@ export class CardColor {
     }
 }
 
-
 new CardColor('red', '#c23331' ,'#fff8db');
 new CardColor('purple', '#BA9ACA', '#000000');
 new CardColor('green', '#95A850', '#000000');
 new CardColor('blue', '#98d4cd', '#000000');
+new CardColor('salmon', '#EFA890', '#000000');
 new CardColor('gray', '#bfb5b3', '#000000');
 new CardColor('black', '#000000', '#fff8db');
