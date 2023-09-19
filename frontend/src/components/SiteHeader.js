165091fefb28780bee8eaa245a62049168ac1b53
jagLas <jglasher@gmail.com>
2023-09-19 11:13:27 -0400

    site Header Styled

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -3,10 +3,12 @@ import './SiteHeader.css'
 export default function SiteHeader () {
     return (
         <div id='site-header'>
-            <img className='App-logo' src='/food-chain-logo.png'></img>
+            <div className='App-logo'></div>
+            <div className='logo-bar'></div>
+            {/* <img className='App-logo' src='/food-chain-logo-no-text.png'></img> */}
             {/* <div className='App-logo'>test</div> */}
             <div id='site-title'>
-                <h1>Helper</h1>
+                <h1>Food Chain Magnate Helper</h1>
             </div>
             
         </div>
