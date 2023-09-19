0103760a82436ebe70e8ecc2748656be11b9eaf9
jagLas <jglasher@gmail.com>
2023-09-19 11:40:15 -0400

    max values for header

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -5,8 +5,6 @@ export default function SiteHeader () {
         <div id='site-header'>
             <div className='App-logo'></div>
             <div className='logo-bar'></div>
-            {/* <img className='App-logo' src='/food-chain-logo-no-text.png'></img> */}
-            {/* <div className='App-logo'>test</div> */}
             <div id='site-title'>
                 <h1>Food Chain Magnate Helper</h1>
             </div>
