89d357a8d65f75b4d93760e427617c2ae51929e7
jagLas <jglasher@gmail.com>
2023-09-19 14:33:51 -0400

    header returns to home on click

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -1,9 +1,16 @@
+import { useNavigate } from 'react-router-dom'
 import './SiteHeader.css'
 
 export default function SiteHeader () {
+    const navigate=useNavigate()
+    
+    const onClickHandler = () => {
+        navigate('')
+    }
+
     return (
         <div id='site-header'>
-            <div className='App-logo'></div>
+            <div className='App-logo' onClick={onClickHandler}></div>
             <div className='logo-bar'></div>
             <div id='site-title'>
                 <h1>Food Chain Magnate Helper</h1>
