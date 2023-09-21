6560997861315b1b72adbe930ae0d2a3ba1f42d1
jagLas <jglasher@gmail.com>
2023-09-21 11:12:08 -0400

    authFetch Wrapper function

--- frontend/src/utilities/auth.js
+++ frontend/src/utilities/auth.js
@@ -4,56 +4,32 @@ function getCookie(name) {
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
-const url = process.env.REACT_APP_DB_URL
-
-const authFetch = async (url, options) => {
-
-}
-
-const login = async () => {
+export const authFetch = async (urlEndpoint, options={method: 'GET'}) => {
+    // wrapper function to add necessary headers and credentials to an authorized request
     const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    const raw = JSON.stringify({
-        "email": "bill@demo.com",
-        "password": "password"
-    });
-
-    const requestOptions = {
-    method: 'POST',
-    credentials: 'include',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
-    };
-
-    try{
-        let data = await fetch(`${process.env.REACT_APP_DB_URL}/auth/login`, requestOptions)
-        data = await data.json()
-        console.log(data)
-        let cookie = getCookie('csrf_access_token')
-        console.log(cookie)
-    } catch(error) {
-        console.log(error)
+    if (options.method != 'GET') {
+        myHeaders.append("Content-Type", "application/json")
+        myHeaders.append("X-CSRF-TOKEN", getCookie('csrf_access_token'))
     }
-}
 
-const logout = async () => {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    const requestOptions = {
-        method: 'POST',
+    let requestOptions = {
+        ...options,
         credentials: 'include',
-        headers: myHeaders,
-        redirect: 'follow'
-        };
-
-    try{
-        let data = await fetch("http://localhost:5000/auth/logout", requestOptions)
-        data = await data.json()
-        console.log(data)
-    } catch(error) {
-        console.log(error)
+        headers: myHeaders
     }
+
+    const res = await (fetch(`${process.env.REACT_APP_DB_URL + urlEndpoint}`, requestOptions))
+    const resData = await res.json()
+
+    if (res.ok) {
+        return resData
+    } 
+    
+    const e = new Error()
+    e.message = resData.description
+    e.statusCode = res.status
+    if (resData.name) {
+        e.name = 'Error-' + resData.name
+    }
+    throw e
 }
