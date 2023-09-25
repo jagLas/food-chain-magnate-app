c1e7396dfb340aee6b79e1af49a5b328eeaceb7c
jagLas <jglasher@gmail.com>
2023-09-25 11:53:07 -0400

    navbar updates

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -1,15 +1,18 @@
-import { useNavigate } from 'react-router-dom'
+import { NavLink, useLocation, useNavigate } from 'react-router-dom'
 import './SiteHeader.css'
 import LogoutButton from './LogoutButton'
 import { useUserContext } from '../App'
 
 export default function SiteHeader () {
     const { isAuthenticated } = useUserContext()
+    const {pathname} = useLocation()
     const navigate=useNavigate()
     
     const onClickHandler = () => {
         navigate('')
     }
+
+    console.log(pathname)
 
     return (
         <div id='site-header'>
@@ -19,7 +22,13 @@ export default function SiteHeader () {
                 <h1>Food Chain Magnate Helper</h1>
                 {isAuthenticated ? <LogoutButton /> : false}
             </div>
-            
+            {pathname !== '/' && <div className='navbar'>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink  to='/games/create-game'>Create Games</NavLink>
+                <NavLink to='/games' end>View Games</NavLink>
+                <NavLink to='/players' end>View Players</NavLink>
+                <NavLink to='/players/create-player'>Create Players</NavLink>
+            </div>}
         </div>
 
     )
