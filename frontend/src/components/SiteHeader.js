76811d7efb9c6f97acce94528c7a5b2a6111dd3c
jagLas <jglasher@gmail.com>
2023-09-25 13:17:26 -0400

    improved site header

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -5,30 +5,30 @@ import { useUserContext } from '../App'
 
 export default function SiteHeader () {
     const { isAuthenticated } = useUserContext()
-    const {pathname} = useLocation()
     const navigate=useNavigate()
     
     const onClickHandler = () => {
         navigate('')
     }
 
-    console.log(pathname)
-
     return (
         <div id='site-header'>
             <div className='App-logo' onClick={onClickHandler}></div>
-            <div className='logo-bar'></div>
-            <div id='site-title'>
-                <h1>Food Chain Magnate Helper</h1>
-                {isAuthenticated ? <LogoutButton /> : false}
+            <div className='header-main'>
+                
+                <div id='site-title'>
+                    <h1>Food Chain Magnate Helper</h1>
+                    {isAuthenticated ? <LogoutButton /> : false}
+                    <div className='logo-bar'></div>
+                </div>
+                <div className='navbar'>
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink  to='/games/create-game'>Create Games</NavLink>
+                    <NavLink to='/games' end>View Games</NavLink>
+                    <NavLink to='/players' end>View Players</NavLink>
+                    <NavLink to='/players/create-player'>Create Players</NavLink>
+                </div>
             </div>
-            {pathname !== '/' && <div className='navbar'>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink  to='/games/create-game'>Create Games</NavLink>
-                <NavLink to='/games' end>View Games</NavLink>
-                <NavLink to='/players' end>View Players</NavLink>
-                <NavLink to='/players/create-player'>Create Players</NavLink>
-            </div>}
         </div>
 
     )
