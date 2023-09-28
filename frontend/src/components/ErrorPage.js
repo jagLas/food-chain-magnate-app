be93f80c75c0fedf3f1ec6659a7ed5f6784dee14
jagLas <jglasher@gmail.com>
2023-09-28 13:17:45 -0400

    404 handling in browser router

--- frontend/src/components/ErrorPage.js
+++ frontend/src/components/ErrorPage.js
@@ -6,7 +6,9 @@ import { useLocation, useNavigate } from "react-router-dom"
 export default function ErrorPage (props) {
     const { setIsAuthenticated } = useUserContext()
     const navigate = useNavigate()
-    const {state} = useLocation()
+    const {state, pathname} = useLocation()
+
+    console.log(pathname)
 
     // useEffect(() => {
     //     const redirect = setTimeout(() => {
@@ -16,10 +18,23 @@ export default function ErrorPage (props) {
     // }, [])
 
     useEffect(() => {
+        // checks if user still has authentication cookies
         if (!checkAuth()) {
             setIsAuthenticated(false)
         }
     }, [setIsAuthenticated])
+
+    if (pathname !== '/error') {
+        return (
+            <>
+            <h1>Page Not Found</h1>
+            <h2>{404}</h2>
+            <h3>The Requested Page Could Not Be Found</h3>
+            <button className="menu-button" onClick={() => navigate(-1)}>Go Back</button>
+            <button className="menu-button" onClick={() => navigate('/')}>Home</button>
+        </>
+        )
+    }
 
     return (
         <>
