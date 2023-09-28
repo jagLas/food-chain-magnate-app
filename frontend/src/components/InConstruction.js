42c210f4f5bc78deea0fee79f486c0688235cb63
jagLas <jglasher@gmail.com>
2023-09-28 18:37:42 -0400

    deleted frontend

--- frontend/src/components/InConstruction.js
+++
@@ -1,8 +0,0 @@
-export default function InConstruction () {
-    return(
-        <>
-            <h2>In Construction!</h2>
-            <p>The page you tried to reach is not ready yet. Please check back later.</p>
-        </>
-    )
-}
